2cf3c6268ca7f219acf88a9ec9b440ab689bdd34
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-04 14:29:29 -0500

    nailed it

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,68 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import WalletAdderForm from './components/WalletAdderForm'
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    wallet: 100,
+    cursor: 1,
+    eatenSushi: []
+  }
+
+  componentDidMount() {
+    fetch(API).then(res => res.json()).then(sushis => this.setState({sushis: sushis}))
+  }
+
+  handleMoreClicked = e => {
+    this.setState(previousState => {
+      let newCursor = previousState.cursor + 4 > this.state.sushis.length ? 
+          1 : previousState.cursor + 4
+      return {
+        cursor: newCursor
+      }
+    })
+  }
+
+  eatSushi = sushi => {
+    let wallet = this.state.wallet
+    if (sushi.price <= wallet) {
+      this.setState({
+        eatenSushi: [...this.state.eatenSushi, sushi],
+        wallet: wallet - sushi.price
+      })
+    }
+  }
+
+  addCash = newBalance => {
+    const cash = parseInt(newBalance)
+
+    if (cash) {
+      this.setState(previousState => {
+        return {
+          wallet: previousState.wallet + cash
+        }
+      })
+    }
+  }
+
   render() {
+    const {cursor, sushis, wallet, eatenSushi} = this.state
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <WalletAdderForm onSubmit={this.addCash} />
+        <SushiContainer 
+          sushis={sushis.slice(cursor-1, (cursor+4)-1)}
+          handleMoreClicked={this.handleMoreClicked}
+          eatSushi={this.eatSushi}
+          eatenSushi={eatenSushi}
+        />
+        <Table wallet={wallet} eatenSushi={eatenSushi}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushis.map((s, i) => (
+              <Sushi 
+              sushi={s} 
+              key={i} 
+              eatenSushi={props.eatenSushi} 
+              eatSushi={props.eatSushi}
+              />
+            ))
         }
-        <MoreButton />
+        <MoreButton handleMoreClicked={props.handleMoreClicked}/>
       </div>
     </Fragment>
   )
